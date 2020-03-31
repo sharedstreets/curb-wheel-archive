a077e8ba732466684f596696c723f88bcfe23048
morganherlocker <morgan.herlocker@gmail.com>
2020-03-31 15:52:41 -0700

    expose server lib; add integration test

--- src/index.js
+++ src/index.js
@@ -3,42 +3,52 @@ const express = require("express");
 const fileUpload = require("express-fileupload");
 const extract = require("./extract");
 
-const app = express();
-
-// constants
-const PORT = 80;
-const TMP_PBF = "./extract.osm.pbf";
-
-// middleware
-app.use(fileUpload());
-
-// application state
-let state = {};
-
 async function main() {
-  app.get("/", (req, res) => res.send("<h1>curb wheel</h1>"));
-  app.post("/extract", async (req, res) => {
-    if (!req.files || Object.keys(req.files).length === 0) {
-      return res.status(400).send("No pbf file was uploaded.");
-    }
+  return new Promise((resolve, reject) => {
+    let app = express();
 
-    let pbf = req.files.pbf;
+    // constants
+    const PORT = 80;
+    const TMP_PBF = "./extract.osm.pbf";
 
-    pbf.mv(TMP_PBF, async function (err) {
-      if (err) {
-        return res.status(500).send(err);
+    // middleware
+    app.use(fileUpload());
+
+    // application state
+    app.state = {};
+
+    app.get("/", (req, res) => res.send("<h1>curb wheel</h1>"));
+    app.post("/extract", async (req, res) => {
+      if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send("No pbf file was uploaded.");
       }
 
-      // extract pbf and build street database
-      // state.graph = await extract()
+      let pbf = req.files.pbf;
 
-      await fs.unlink(TMP_PBF);
+      pbf.mv(TMP_PBF, async function (err) {
+        if (err) {
+          return res.status(500).send(err);
+        }
 
-      res.status(200).send("Extract complete.");
+        // extract pbf and build street database
+        // app.state.graph = await extract()
+
+        await fs.unlink(TMP_PBF);
+
+        res.status(200).send("Extract complete.");
+      });
+    });
+
+    let server = app.listen(PORT, () => {
+      return resolve(server);
     });
   });
-
-  app.listen(PORT, () => console.log("curbwheel server started"));
 }
 
-main();
+if (require.main === module) {
+  // cli
+  main();
+} else {
+  // lib
+  module.exports = main;
+}
