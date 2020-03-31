146f38269025a9eb5a0999b4af1a9d245c1cefa7
morganherlocker <morgan.herlocker@gmail.com>
2020-03-31 13:31:19 -0700

    rm sample upload code

--- src/index.js
+++ src/index.js
@@ -38,18 +38,6 @@ async function main() {
     });
   });
 
-  app.post("/upload", function (req, res) {
-    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-    let sampleFile = req.files.sampleFile;
-
-    // Use the mv() method to place the file somewhere on your server
-    sampleFile.mv("/somewhere/on/your/server/filename.jpg", function (err) {
-      if (err) return res.status(500).send(err);
-
-      res.send("File uploaded!");
-    });
-  });
-
   app.listen(PORT, () => console.log("curbwheel server started"));
 }
 
