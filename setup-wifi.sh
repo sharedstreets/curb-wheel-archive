e2c45bfdda837b34fdced36c44fc57f3c8f1665e
Kevin Webb <kpwebb@gmail.com>
2020-04-08 13:14:15 -0400

    correct etc/wpa_supplicant path

--- setup-wifi.sh
+++ setup-wifi.sh
@@ -1,4 +1,4 @@
 # edit config/wpa_supplicant.conf.template 
 # file to include local network name and password
 
-sudo cp config/wpa_supplicant.conf.template /etc/wpa/wpa_supplicant.conf
+sudo cp config/wpa_supplicant.conf.template /etc/wpa_supplicant/wpa_supplicant.conf
