a35c197fd3a9c6c856bcf7bb9d0afd5a495a5e9d
peterqliu <peterqliu@gmail.com>
2020-04-28 14:37:33 -0700

    refactor ui into overview.html

---
+++ static/map.js
@@ -0,0 +1,135 @@
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibW9yZ2FuaGVybG9ja2VyIiwiYSI6Ii1zLU4xOWMifQ.FubD68OEerk74AYCLduMZQ';
+
+app.ui.map = new mapboxgl.Map({
+	container: 'map',
+	style: 'mapbox://styles/mapbox/light-v10',
+	hash: true
+});
+
+app.ui.map
+	.setZoom(16)
+	.setCenter(app.devMode.sampleStreet.geometry.coordinates[0])
+	.on('load', () => {
+
+			app.ui.map
+				.addLayer({
+					'id': 'streets', 
+					'type':'line', 
+					'source':{
+						type: 'geojson', 
+						data: app.constants.emptyGeojson
+					},
+					'paint':{
+						'line-width':10,
+						'line-opacity':0.2
+					}
+				})
+				.addLayer({
+					'id': 'arrows', 
+					'type':'symbol', 
+					'source':{
+						type: 'geojson', 
+						data: app.constants.emptyGeojson
+					},
+					'layout': {
+						'text-keep-upright': false,
+						'text-field': '{direction} → ',
+						'symbol-placement': 'line',
+						'symbol-spacing':{
+							stops:[[16,20],[22,80]]
+						},
+						'text-size':{
+							'stops':[[16, 12], [22, 45]]
+						},
+						'text-allow-overlap': true,
+						'text-ignore-placement': true
+					},
+					'paint':{
+						'text-color':{
+							property: 'direction',
+							type: 'categorical',
+							stops:[['A', 'orangered'], ['B', 'green']]
+						}
+					}
+				})
+				.addLayer({
+					'id': 'curbs', 
+					'type':'line', 
+					'source':{
+						type: 'geojson', 
+						data: {type:'FeatureCollection', features:[]}
+					},
+					'paint':{
+						'line-width': 3
+					}					
+				})
+				.on('click', 'streets', (e)=>{
+
+					if (app.state.mode = 'selectStreet'){
+
+						var edge = e.features[0].geometry.coordinates;
+						app.state.street = e.features[0].properties;
+						app.ui.map
+							.fitBounds(edge, {padding:60})
+
+						var forward = turf.lineOffset(turf.lineString(edge, {direction:'A', ref: e.features[0].properties.forward}), 10, {units: 'meters'});
+						edge.reverse();
+						var backward = turf.lineOffset(turf.lineString(edge, {direction:'B', ref: e.features[0].properties.back}), 10, {units: 'meters'});
+						
+						app.ui.map.getSource('arrows')
+							.setData({type:'FeatureCollection', features:[forward, backward]})
+
+						app.ui.mode.set('selectDirection')
+					}
+
+				})
+				.on('click', 'arrows', (e)=>{
+
+					if (app.state.mode = 'selectDirection'){
+
+						var ref = e.features[0].properties.ref;
+
+						app.ui.confirm(
+							app.constants.prompts.beginSurvey, 
+							()=>{
+								app.state.street.ref = ref;
+								app.state.street.distance = 122;
+								app.state.currentRollDistance = 0;
+
+								app.ui.mode.set('rolling');
+								app.devMode.rolling= true;
+							}
+						)
+
+
+					}
+
+				})
+				.on('moveend', e => {
+					let zoom = app.ui.map.getZoom()
+					if (zoom >= 14) {
+						let viewport = app.ui.map.getBounds()
+
+						let url = '/query?';
+						url += 'minX=' + viewport._sw.lng
+						url += '&minY=' + viewport._sw.lat
+						url += '&maxX=' + viewport._ne.lng
+						url += '&maxY=' + viewport._ne.lat
+
+						fetch(url)
+							.then((response) => {
+							return response.json();
+							})
+							.then((streets) => {
+								app.ui.map.getSource('streets').setData(streets);
+							});
+							} else {
+							map.getSource('streets').setData(app.constants.emptyGeojson);
+					}
+				})
+
+	})
+
+// app.ui.map.showCollisionBoxes = true
+// app.ui.map.showTileBoundaries = true
