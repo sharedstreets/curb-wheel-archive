9f2ce50528321158de0d041471197bafcc010c39
morganherlocker <morgan.herlocker@gmail.com>
2020-06-03 16:18:05 -0700

    custom bench implementation

--- bench/extract.bench.js
+++ bench/extract.bench.js
@@ -1,43 +1,37 @@
 const path = require("path");
-const Benchmark = require("benchmark");
-const Graph = require("./src/graph")
+const Graph = require("../src/graph");
 
-function bench () {
-  let suite = new Benchmark.Suite;
+async function cycle(fixture) {
+  let start = Date.now();
 
-  // add tests
-  suite.add('small', async () => {
-    // small
-    const pbf = path.join(__dirname, "./test/fixtures/honolulu.osm.pbf");
+  let graph = new Graph();
+  await graph.extract(fixture);
 
-    let graph = new Graph();
-
-    await graph.extract(pbf);
-  })
-  .add('medium', function() {
-    // medium
-    const pbf = path.join(__dirname, "./test/fixtures/honolulu.osm.pbf");
-
-    let graph = new Graph();
-
-    await graph.extract(pbf);
-  })
-  .add('large', function() {
-    // large
-    const pbf = path.join(__dirname, "./test/fixtures/honolulu.osm.pbf");
-
-    let graph = new Graph();
-
-    await graph.extract(pbf);
-  })
-  // add listeners
-  .on('cycle', function(event) {
-    console.log(String(event.target));
-  })
-  .on('complete', function(res) {
-    console.log(result);
-  })
-  .run()
+  let stop = Date.now();
+  return stop - start;
 }
 
-bench()
+async function bench(fixture, description) {
+  console.log(path.basename(fixture));
+  console.log(description)
+  console.log("---");
+
+  let cycles = 10;
+  let total = 0;
+  for (let i = 0; i < cycles; i++) {
+    let time = await cycle(fixture);
+    console.log((i + 1), "/", cycles, ": ", time, "ms");
+    total += time;
+  }
+
+  console.log("average: ", (total / cycles).toFixed(4) + " ms\n");
+}
+
+async function run () {
+  await bench(path.join(__dirname, "../test/fixtures/oakland.osm.pbf"), "small neighborhood transport-only hot extract");
+  await bench(path.join(__dirname, "../test/fixtures/honolulu.osm.pbf"), "medium full city nextzen metro extract");
+  await bench(path.join(__dirname, "../test/fixtures/dc.osm.pbf"), "medium full city transport-only hot extract");
+  await bench(path.join(__dirname, "../test/fixtures/nyc.osm.pbf"), "large full city transport-only hot extract");
+}
+
+run();
