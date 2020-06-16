02764dad0f1cac31db5c6cf8afd99da4cefcbf69
peterqliu <peterqliu@gmail.com>
2020-06-16 15:44:44 -0700

    style changes

--- static/map.js
+++ static/map.js
@@ -34,7 +34,7 @@ app.ui.map
 				},
 				layout: {
 					"text-keep-upright": false,
-					"text-font": ["Noto Sans Regular"],
+					// "text-font": ["Noto Sans Regular"],
 					"text-field": app.constants.mapStyle.arrows.direction.forward,
 					"symbol-placement": "line",
 					"symbol-spacing": {
@@ -57,14 +57,7 @@ app.ui.map
 					}
 				},
 				paint: {
-					"text-color": {
-						property: "direction",
-						type: "categorical",
-						stops: [
-							["A", "orangered"],
-							["B", "green"],
-						],
-					},
+					"text-color": 'steelblue',
 				},
 			})
 			.addLayer({
